2b2667dd21d56acc015ab9bfc232dedfc4fec8df
Aliciaho <E0425365@u.nus.edu>
2020-08-25 15:30:25 +0800

    Increment: level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println(" ");
+        line = in.nextLine().toLowerCase();
+        while(!line.equals("bye")){
+            System.out.println(line);
+            line = in.nextLine().toLowerCase();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
